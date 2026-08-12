cbb6da7f3560af5058885e871900da4329bfc5d3
NicolasPKSescola <nicolas.kmick.santos@escola.pr.gov.br>
2026-08-12 16:34:20 -0300

    cadastro

---
+++ server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 5500;
+
+app.use(express.json());
+app.use(cors());
+
+const user = [];
+
+app.get('/', (req, res)=>{
+    return res.json(user);
+})
+
+app.post('/cadastro', (req, res)=>{
+    const {usuario, email, senha} = req.body;
+
+    if (!usuario || !email || !senha) {
+        return res.status(400).json({
+            erro: "Preencha todos os campos"
+        });
+    }
+
+    if (senha.length < 6) {
+        return res.status(400).json({
+            erro: "A senha deve ter 6 caracteres ou mais"
+        });
+    }
+
+    const emailUsado = user.find((busca) => busca.email === email);
+
+    if (emailUsado) {
+        return res.status(409).json({
+            erro: "Email já está em uso"
+        });
+    }
+
+    console.log(`Recebido, aguarde 5 segundo`);
+
+    user.push(usuario, email, senha);
+
+    setTimeout(()=>{
+        console.log(`Cadastrado o ${usuario} com o Email ${email}`);
+
+        res.json({
+            mensagem: "Cadastrado com Sucesso!!",
+            perfil: `Perfil ${usuario} cadastrado com o email ${email}`
+        });
+    }, 5000);
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
