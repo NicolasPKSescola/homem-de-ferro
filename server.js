8c4519798a6e6e2338e9ec442a3999fb5119a761
NicolasPKSescola <nicolas.kmick.santos@escola.pr.gov.br>
2026-09-01 21:02:20 +0000

    env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const PORT = 5500;
+const PORT = process.env.PORT || 5500;
 
 app.use(express.json());
 app.use(cors());
